ff68758d4bdfcc5b872e5d38ac93ddb84075be22
karovh <carolinavillamilh@gmail.com>
2022-10-25 10:48:05 -0500

    Pipe Imagen, Creacion Ruta Heroe, Diseño pantalla heroe, Variables de entorno, Material Autocomplete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ErrorPageComponent
+
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    HttpClientModule
 
   ],
   providers: [],
